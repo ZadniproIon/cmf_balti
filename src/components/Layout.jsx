8111abbda947181beba2e28374067228078dd93e
ZadniproIon <nutzugt@gmail.com>
2026-01-06 00:32:10 +0200

    Added Inter font and removed topbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,9 @@
 ﻿import { Outlet } from 'react-router-dom'
-import AboveHeader from './AboveHeader'
 import Footer from './Footer'
 import Navbar from './Navbar'
 
 const Layout = () => (
   <>
-    <AboveHeader />
     <Navbar />
     <Outlet />
     <Footer />
