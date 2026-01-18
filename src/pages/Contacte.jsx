ec30bf1ecd7a02da9697803f6c819ad4079f50ed
ZadniproIon <nutzugt@gmail.com>
2026-01-18 02:57:49 +0200

    Added scrollWheelZoom to map

--- src/pages/Contacte.jsx
+++ src/pages/Contacte.jsx
@@ -238,7 +238,7 @@ const Contacte = () => {
       </div>
 
       <div id="contact-right-side">
-        <MapContainer className="contact-map" center={centerCmf1} zoom={12} scrollWheelZoom={false}>
+        <MapContainer className="contact-map" center={centerCmf1} zoom={12} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
